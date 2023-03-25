b15df8d4852351572260f61c145c83691a1a075f
mario <elplanetadefoxy@gmail.com>
2023-03-25 19:21:41 -0300

    2da entrega

--- src/containers/Item.js
+++ src/containers/Item.js
@@ -13,24 +13,23 @@ const Item = ({product}) => {
                }, 2000);
            } 
     return(
-        <> 
-       
-    <Card className="card" >
-      <CardActionArea className="cardA">
-        <CardMedia className="cardmed" component="img" height="140" image={product.image} alt={product.title}/>
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div"> {product.title} </Typography>
-          <Typography variant="body2" color="text.secondary"> ${product.price} </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions className="cardAc">
-        <ItemCount className="contador" stock = {10} onAdd={onAdd}/>
-        <Link to={`/product/${product.id}`}>
-          <Button size="small" color="primary"> Ver Detalles </Button>
-        </Link>
-      </CardActions>
-    </Card>
-    </> 
+        <>  
+            <Card className="card" >
+                <CardActionArea className="cardA">
+                    <CardMedia className="cardmed" component="img" height="140" image={product.image} alt={product.title}/>
+                        <CardContent>
+                            <Typography gutterBottom variant="h6" component="div"> {product.title} </Typography>
+                            <Typography variant="body2" color="text.secondary"> ${product.price} </Typography>
+                        </CardContent>
+                </CardActionArea>
+                <CardActions className="cardAc">
+                    <ItemCount className="contador" stock = {10} onAdd={onAdd}/>
+                    <Link to={`/product/${product.id}`}>
+                        <Button size="small" color="primary"> Ver Detalles </Button>
+                    </Link>
+                </CardActions>
+            </Card>
+        </> 
     )
 };
 
